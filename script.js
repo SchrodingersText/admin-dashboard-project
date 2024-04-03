5732ce6ce3073d410761664ec6fc415bc7b79818
SchrodingersText <130055324+SchrodingersText@users.noreply.github.com>
2024-04-03 15:59:41 +0200

    Style side feature section for mobile

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ const showMenu = function () {
 }
 
 hamburgerMenu.addEventListener("click", showMenu)
+
+const expandButton = document.getElementById("expand-button");
+const collapseButton = document.getElementById("collapse-button");
+const sideFeatures = document.getElementById("side-features");
+
+const expandSection = function () {
+   sideFeatures.classList.add("expanded");
+   sideFeatures.classList.remove("fade");
+   expandButton.classList.remove("visible");
+   collapseButton.classList.add("visible");
+}
+
+const collapseSection = function () {
+   sideFeatures.classList.remove("expanded");
+   sideFeatures.classList.add("fade");
+   expandButton.classList.add("visible");
+   collapseButton.classList.remove("visible");
+}
+
+expandButton.addEventListener("click", expandSection);
+collapseButton.addEventListener("click", collapseSection);
